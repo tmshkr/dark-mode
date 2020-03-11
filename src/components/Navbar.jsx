078fb499079149eaaadc16bfbc31466b4e76b977
Tim Shaker <tim@tmshkr.com>
2020-03-11 12:25:14 -0700

    move onClick to parent div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useDarkMode } from "../hooks/useDarkMode";
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  const [darkMode, toggleMode] = useDarkMode();
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
-      <div className="dark-mode__toggle">
-        <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
-        />
+      <div className="dark-mode__toggle" onClick={toggleMode}>
+        <div className={darkMode ? "toggle toggled" : "toggle"} />
       </div>
     </nav>
   );

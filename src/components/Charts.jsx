cf3708d44d0c7c575a7851e6b2d981821fe24434
Tim Shaker <tim@tmshkr.com>
2020-03-11 13:34:53 -0700

    add dropdown menu

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Chart from "./Chart";
 
 const Charts = ({ coinData }) => {
+  const [activeCoin, setActiveCoin] = useState(0);
+  const coin = coinData[activeCoin];
+  if (!coin) return <h2>Loading...</h2>;
   return (
     <div className="charts">
-      {coinData.map(coin => (
-        <div className="chart__container" key={coin.name}>
-          <h2 className="coin__title">{coin.name}</h2>
-          <h4 className="coin__symbol">{coin.symbol}</h4>
-          <div className="coin__logo">
-            <img src={coin.image} height="40" alt={coin.name} />
-          </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} />
+      <div className="chart__container" key={coin.name}>
+        <h2 className="coin__title">
+          {coin.name} ({coin.symbol.toUpperCase()})
+        </h2>
+        <div className="coin__logo">
+          <img src={coin.image} height="40" alt={coin.name} />
         </div>
-      ))}
+        <Chart sparklineData={coin.sparkline_in_7d.price} />
+      </div>
+      <select onChange={e => setActiveCoin(e.target.value)}>
+        {coinData.map((coin, index) => (
+          <option key={coin.name} value={index}>
+            {coin.name}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };

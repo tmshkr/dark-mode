0e8fbeb25a4fff3eac0fd73fbe809c42eea376f1
Tim Shaker <tim@tmshkr.com>
2020-03-11 12:24:04 -0700

    useDarkMode hook

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useDarkMode = () => {
+  const [storedValue, setValue] = useLocalStorage("dark-mode", false);
+  const [darkMode, setDarkMode] = useState(storedValue);
+  const toggleMode = () => {
+    setDarkMode(!darkMode);
+    setValue(!darkMode);
+    darkMode
+      ? document.body.classList.remove("dark-mode")
+      : document.body.classList.add("dark-mode");
+  };
+
+  // load initial state from localStorage
+  useEffect(() => {
+    console.log("useEffect");
+    darkMode
+      ? document.body.classList.add("dark-mode")
+      : document.body.classList.remove("dark-mode");
+  }, []);
+
+  return [darkMode, toggleMode];
+};
